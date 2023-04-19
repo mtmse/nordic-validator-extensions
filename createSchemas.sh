1f474ebcb7cfa2595ac2691abc26d0bfbd733831
Daniel Persson <mailto.woden@gmail.com>
2023-04-19 11:55:35 +0200

    Added nordic rules for DTBook.

--- createSchemas.sh
+++ createSchemas.sh
@@ -5,6 +5,7 @@ SCHXSLT_PATH=schxslt
 mkdir -p src/main/resources/dtbook/compiled
 saxon11-xslt -s:src/main/resources/dtbook/sch/dtbook.mathml.nimas.sch -o:src/main/resources/dtbook/compiled/dtbook.mathml.nimas.xsl -xsl:$SCHXSLT_PATH/src/main/resources/content/transpile.xsl
 saxon11-xslt -s:src/main/resources/dtbook/sch/dtbook.mathml.sch -o:src/main/resources/dtbook/compiled/dtbook.mathml.xsl -xsl:$SCHXSLT_PATH/src/main/resources/content/transpile.xsl
+saxon11-xslt -s:src/main/resources/dtbook/sch/mtm2015-1.sch -o:src/main/resources/dtbook/compiled/mtm2015-1.xsl -xsl:$SCHXSLT_PATH/src/main/resources/content/transpile.xsl
 
 mkdir -p src/main/resources/daisy3/compiled
 saxon11-xslt -s:src/main/resources/daisy3/sch/daisy3.opf.sch -o:src/main/resources/daisy3/compiled/daisy3.opf.xsl -xsl:$SCHXSLT_PATH/src/main/resources/content/transpile.xsl
--- src/main/java/org/daisy/validator/schemas/GuidelineDTBookNordic.java
+++ src/main/java/org/daisy/validator/schemas/GuidelineDTBookNordic.java
@@ -3,7 +3,7 @@ package org.daisy.validator.schemas;
 public class GuidelineDTBookNordic extends GuidelineDTBook {
     public GuidelineDTBookNordic() {
         super();
-        schemaMap.put(CONTENT_FILES, new Schema("nordic2015-1.xsl", "Nordic HTML (DTBook Content Document)", ""));
+        schemaMap.put(CONTENT_FILES, new Schema("compiled/mtm2015-1.xsl", "Nordic HTML (DTBook Content Document)", ""));
         schemaMap.put(DTBOOKNORDIC2005_3, new Schema("rng/nordic-dtbook-2005-3.rng", "DTBook 2005-3 Nordic rules", ""));
     }
 }
