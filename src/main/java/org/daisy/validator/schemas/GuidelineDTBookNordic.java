49fb5fa14adb5d7c674cde07206aed1ae8a22945
Daniel Persson <mailto.woden@gmail.com>
2023-04-19 10:39:55 +0200

    MathML fix.

--- src/main/java/org/daisy/validator/schemas/GuidelineDTBookNordic.java
+++ src/main/java/org/daisy/validator/schemas/GuidelineDTBookNordic.java
@@ -3,6 +3,6 @@ package org.daisy.validator.schemas;
 public class GuidelineDTBookNordic extends GuidelineDTBook {
     public GuidelineDTBookNordic() {
         super();
-        schemaMap.put(DTBOOKNORDIC2005_3, new Schema("rng/dtbook/nordic-dtbook-2005-3.rng", "DTBook 2005-3 Nordic rules", ""));
+        schemaMap.put(DTBOOKNORDIC2005_3, new Schema("rng/nordic-dtbook-2005-3.rng", "DTBook 2005-3 Nordic rules", ""));
     }
 }
